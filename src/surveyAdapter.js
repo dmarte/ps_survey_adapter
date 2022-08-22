f2d50f0f1f8aa5c24cd7fab4b4e80dab7d6ff97a
Delvi Marte <delvi.marte@gmail.com>
2022-08-22 15:53:35 -0400

    AD22-174 Partial commit: Starting to implement button on floating ads.

--- src/surveyAdapter.js
+++ src/surveyAdapter.js
@@ -172,7 +172,7 @@ import { PSDom } from './PSDom.js';
         button.addEventListener('click', () => {
           window.open(this.getUrl(), '_blank');
         });
-console.log(button.renderOnFloating);
+console.dir(button);
         // [STEP 5] - Draw in the wrapper
         if (PSDom.enabled(tag, 'floating')) {
           button.renderOnFloating();
